3cfeda9a15d668ca9c939f44dab52e58b7501fa6
Shannon Cwikla <socwikla@gmail.com>
2017-11-06 16:58:58 -0600

    committing configuration complete

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 const port = 3000;
 
+app.use(function(req,res,next){
+    console.log(`${req.method} ${req.path}`);
+    next();
+});
+
+app.use('/special/', function(req,res,next){
+    console.log('you reached the special area');
+    next();
+});
+
 app.get('/',(req,res) => res.send('Welcome'));
 
 app.listen(port, function() {console.log('server listening')});
