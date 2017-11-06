aaca86184c236ad0c6b140b23a808fd3bb6178bf
Shannon Cwikla <socwikla@gmail.com>
2017-11-06 17:34:57 -0600

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-const express = require( 'express' );
+const express = require('express');
+const nunjucks = require('nunjucks');
 const app = express(); // creates an instance of an express application
 const port = 3000;
+
+// nunjucks
+// app.set('view engine','html')
+// app.engine('html', nunjucks.renderFile);
+// nunjucks.configure('views');
+
+// in some file that is in the root directory of our application... how about app.js?
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+});
+
 
 app.use(function(req,res,next){
     console.log(`${req.method} ${req.path}`);
